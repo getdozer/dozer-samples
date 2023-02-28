62e40a31d96b5769c057c4f7f9b0cee3633f1434
VG <vivekg342@gmail.com>
2023-02-28 16:27:31 +0800

    chore: fix init script

--- pg-flights/data/init.sql
+++ pg-flights/data/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dac5920d4feec6d46a562f7cbc47a73d291c12c38432a7279e7975f484e0d3ef
-size 103865140
+oid sha256:900f33e0ce6f81c6d76a2163f50974e6f974a8b51d19d93e3d26b75f2750b36c
+size 103865374
