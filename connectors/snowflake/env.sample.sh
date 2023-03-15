50b16dc752ba381f0919234e0fca8dec030a4a4f
VG <vivekg342@gmail.com>
2023-03-15 15:42:10 +0800

    feat: introduce snowflake

---
+++ connectors/snowflake/env.sample.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export SN_SERVER=test.snowflakecomputing.com
+export SN_USER="snowflake_user"
+export SN_PASSWORD="snowflake_password"
+export SN_DATABASE=DOZER_SAMPLE_DATA
+export SN_WAREHOUSE=COMPUTE_WH
