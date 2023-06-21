56e4a0cc9726c365929f69aef91265c19dee9eee
Abhishek Mishra <geekabhi@outlook.com>
2023-06-21 15:42:05 +0530

    fix: fix date range issue for dataset generation
    Signed-off-by: Abhishek Mishra <geekabhi@outlook.com>

--- connectors/aws-s3/create_dataset_and_upload_to_s3.py
+++ connectors/aws-s3/create_dataset_and_upload_to_s3.py
@@ -8,21 +8,19 @@ import pandas as pd
 from rich.progress import Progress
 
 
-def create_dataset(file_name, num_rows, month, year):
+def create_dataset(file_name, num_rows, start_date, end_date):
     """Create a dataset"""
     with Progress() as progress:
         task = progress.add_task("[cyan]Creating dataset...", total=num_rows)
 
-        # Generate dates from 2011 to the specified month and year
-        start_date = datetime(2024, 1, 1)
-        end_date = datetime(year, month, 1) - timedelta(days=1)
+        # Generate dates from the specified start date to the specified end date
         date_range = pd.date_range(start_date, end_date).tolist()
 
         # Generate dates
         dates = [random.choice(date_range) for _ in range(num_rows)]
 
         # Generate random tickers
-        tickers = np.random.choice(["AAPL", "GOOG", "MSFT", "AMZN", "FB"], num_rows)
+        tickers = np.random.choice(['AAPL', 'GOOG', 'MSFT', 'AMZN', 'FB'], num_rows)
 
         # Generate random open, high, low, close prices and volumes
         opens = np.random.uniform(100, 200, num_rows)
@@ -32,17 +30,15 @@ def create_dataset(file_name, num_rows, month, year):
         volumes = np.random.randint(5000, 10000, num_rows)
 
         # Create a DataFrame
-        df = pd.DataFrame(
-            {
-                "Date": dates,
-                "Ticker": tickers,
-                "Open": opens,
-                "High": highs,
-                "Low": lows,
-                "Close": closes,
-                "Volume": volumes,
-            }
-        )
+        df = pd.DataFrame({
+            'Date': dates,
+            'Ticker': tickers,
+            'Open': opens,
+            'High': highs,
+            'Low': lows,
+            'Close': closes,
+            'Volume': volumes
+        })
 
         # Write the DataFrame to a CSV file
         df.to_csv(file_name, index=False)
@@ -84,13 +80,26 @@ if __name__ == "__main__":
     if not file_name.endswith(".csv"):
         file_name += ".csv"
     num_rows = int(input(f"Enter the number of rows (default: 10000000): ") or 10000000)
-    month = int(input(f"Enter the end month (1-12): "))
-    year = int(input(f"Enter the end year (default: 2023): ") or 2023)
+
+    while True:
+        start_year = int(input(f'Enter the start year (default: 2022): ') or 2022)
+        start_month = int(input(f'Enter the start month (1-12, default: 1): ') or 1)
+        end_year = int(input(f'Enter the end year (default: 2024): ') or 2024)
+        end_month = int(input(f'Enter the end month (1-12, default: 1): ') or 1)
+
+        start_date = datetime(start_year, start_month, 1)
+        end_date = datetime(end_year, end_month, 1) - timedelta(days=1)
+
+        if start_date >= end_date:
+            print("End date must be later than start date. Please try again.")
+        else:
+            break
+
 
     start_time = time.time()
-    create_dataset(file_name=file_name, num_rows=num_rows, month=month, year=year)
+    create_dataset(file_name=file_name, num_rows=num_rows, start_date=start_date, end_date=end_date)
     end_time = time.time()
-    print(f"Execution time of create_dataset: {end_time - start_time:.2f} seconds")
+    print(f'Execution time of create_dataset: {end_time - start_time:.2f} seconds')
 
     start_time = time.time()
     upload_dataset_to_s3(file_name=file_name)
