1d9291da73d72b8707827e2dec81618e4cb0a72f
jianxiang <xunjianxiang@getdozer.io>
2023-09-28 00:43:40 +0800

    feat: vue sample (#71)
    * feat: add vue sample
    
    * chore: update dozer deps
    
    * chore: update dozer libraries

---
+++ usecases/vue/src/components/EmptyCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="empty-card">
+    No Data
+  </div>
+</template>
+
+<style scoped>
+.empty-card {
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  user-select: none;
+  border: 1px solid var(--color-border);
+  width: 100%;
+  border-radius: 4px;
+}
+</style>
